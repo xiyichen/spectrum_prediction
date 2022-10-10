83fb34d35a6980954bfff9af9fab274eab3fe234
Xiyi Chen <50854238+xiyichen@users.noreply.github.com>
2022-10-10 22:10:40 +0200

    ml methods

--- ml_methods.py
+++ ml_methods.py
@@ -1,124 +1,111 @@
-from torch.utils.data import Dataset
-import glob
-import pandas as pd
-import json
-import numpy as np
-import torch
+from utils import *
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+import xgboost as xgb
+import lightgbm as lgb
+import joblib
 
-# split the lists of wavelengths and optical properties into rows of single values
-def explode(df, lst_cols, fill_value=''):
-    # source: https://stackoverflow.com/questions/45846765/efficient-way-to-unnest-explode-multiple-list-columns-in-a-pandas-dataframe
-    # make sure `lst_cols` is a list
-    if lst_cols and not isinstance(lst_cols, list):
-        lst_cols = [lst_cols]
-    # all columns except `lst_cols`
-    idx_cols = df.columns.difference(lst_cols)
+# Train a model with given training set. The validation set is only applicable for lightgbm.
+def train(X_train, y_train, model_name, X_val=None, y_val=None):
+  if model_name == 'xgboost':
+    dtrain = xgb.DMatrix(X_train, label=y_train)
+    params = {
+        'max_depth': 35,
+        'min_child_weight': 5,
+        'subsample': 0.5,
+        'colsample_bytree': 1,
+        'eta': 0.06
+    }
 
-    # calculate lengths of lists
-    lens = df[lst_cols[0]].str.len()
+    best_model = xgb.train(
+      params,
+      dtrain,
+      feval=mse,
+      num_boost_round=1000,
+    )
 
-    if (lens > 0).all():
-        # ALL lists in cells aren't empty
-        return pd.DataFrame({
-            col:np.repeat(df[col].values, df[lst_cols[0]].str.len())
-            for col in idx_cols
-        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
-          .loc[:, df.columns]
+    best_model.save_model('./checkpoint/' + model_name + '.model')
+    return
+  elif model_name == 'lightgbm':
+    dtrain = lgb.Dataset(X_train, label=y_train)
+    dval = lgb.Dataset(X_val, label=y_val)
+    params = {
+        # fixed params
+        'metric': 'l2',
+        'bagging_freq': 1,
+        'feature_fraction': 1,
+        'max_depth': -1,
+        'min_child_samples': 0,
+        'verbosity': -1,
+        'bagging_fraction': 0.4,
+        'boosting': 'gbdt',
+        # optimized params:
+        'num_iterations': 5000,
+        'learning_rate': 0.1,
+        'max_bin': 200,
+        'num_leaves': 500,
+    }
+
+    best_model = lgb.train(
+      params,
+      dtrain,
+      valid_sets=[dval],
+      early_stopping_rounds=30
+    )
+
+    best_model.save_model('./checkpoint/' + model_name + '.model')
+  if model_name == 'knn':
+    model = KNeighborsRegressor(metric='euclidean', n_neighbors=9, weights='distance', leaf_size=21)
+  elif model_name == 'rf':
+    model = RandomForestRegressor(n_estimators=100, max_depth=31, min_samples_split=2, min_samples_leaf=1, bootstrap=True, verbose=2, n_jobs=-1)
+  elif model_name == 'svr':
+    model = SVR()
+  model.fit(X_train, y_train)
+  joblib.dump(model, './checkpoint/' + model_name + '.joblib')
+
+# Test a model with given test set.
+def test(X_test, y_test, model_name):
+  if model_name == 'xgboost':
+    dtest = xgb.DMatrix(X_test, label=y_test)
+    model = xgb.Booster()
+    model.load_model('./checkpoint/' + model_name + '.model')
+    spectrum_predicteds = model.predict(dtest)
+  elif model_name == 'lightgbm':
+    model = lgb.Booster(model_file='./checkpoint/' + model_name + '.model')
+    spectrum_predicteds = model.predict(X_test)
+  else:
+    model = joblib.load('./checkpoint/' + model_name + '.joblib')
+    spectrum_predicteds = model.predict(X_test)
+  num_points = get_num_points()
+  spectrum_predicteds = np.reshape(spectrum_predicteds, (-1, num_points))
+  y_test = np.reshape(y_test, (-1, num_points))
+  spectrum_rmses = []
+  spectrum_maes = []
+  for i in range(len(spectrum_predicteds)):
+    spectrum_rmses.append(rmse(spectrum_predicteds[i], y_test[i]))
+    spectrum_maes.append(mae(spectrum_predicteds[i], y_test[i]))
+  return spectrum_predicteds, y_test, spectrum_rmses, spectrum_maes
+
+if __name__ == '__main__':
+  if not args.test:
+    # lightgbm takes a validation set
+    if args.ml_method == 'lightgbm':
+      X_train, y_train = load_train_data()
+      X_val, y_val = load_val_data()
+      train(X_train, y_train, args.ml_method, X_val, y_val)
+    # other models only takes a trainig set
     else:
-        # at least one list in cells is empty
-        return pd.DataFrame({
-            col:np.repeat(df[col].values, df[lst_cols[0]].str.len())
-            for col in idx_cols
-        }).assign(**{col:np.concatenate(df[col].values) for col in lst_cols}) \
-          .append(df.loc[lens==0, idx_cols]).fillna(fill_value) \
-          .loc[:, df.columns]
-
-# load JSON files into pandas dataframe
-def make_from_JSON(num_layers, path, wavelength_range, interval):
-    wavelength_l, wavelength_h = wavelength_range
-    num_json = len(glob.glob1(path,'**'))
-    cols = []
-    filenames = []
-    for i in range(1, num_layers+1):
-        cols.append('mat_' + str(i))
-    for i in range(1, num_layers+1):
-        cols.append('t_' + str(i))
-    cols.append('transmission')
-    cols.append('reflection')
-    cols.append('wavelength')
-    df = pd.DataFrame(columns=cols, index = range(num_json))
-    file_idx = 0
-    ID = 100 + num_layers - 2
-    for filename in glob.glob(path + str(ID) + '*'):
-        data = json.load(open(str(filename)))
-        for i in range(1, num_layers+1):
-            df.loc[file_idx]['mat_' + str(i)] = data['geometry']['materials'][i-1]
-            df.loc[file_idx]['t_' + str(i)] = data['geometry']['thickness'][i-1]
-        df.loc[file_idx]['transmission'] = []
-        df.loc[file_idx]['reflection'] = []
-        df.loc[file_idx]['wavelength'] = []
-        filenames.append(filename.split('\\')[-1])
-        for i in range(len(data['opticalProp']['wavelength_nm'])):
-            if (data['opticalProp']['wavelength_nm'][i] - wavelength_l)% interval == 0:
-                df.loc[file_idx]['transmission'].append(data['opticalProp']['transmission'][i])
-                df.loc[file_idx]['reflection'].append(data['opticalProp']['reflection'][i])
-                df.loc[file_idx]['wavelength'].append(data['opticalProp']['wavelength_nm'][i])
-        df.loc[file_idx]['transmission'] = np.array(df.loc[file_idx].transmission)
-        df.loc[file_idx]['reflection'] = np.array(df.loc[file_idx].reflection)
-        df.loc[file_idx]['wavelength'] = np.array(df.loc[file_idx].wavelength)
-        file_idx += 1
-    return df, filenames
-
-# encode the materials combinations into one-hot vectors
-def one_hot_encoding(num_layers, df):
-    cols = []
-    prefixes = []
-    for i in range(1, num_layers+1):
-        cols.append('mat_' + str(i))
-        prefixes.append('mat' + str(i))
-    dummies = pd.get_dummies(df, columns=cols, prefix=prefixes)
-    return dummies
-
-# rescale the features into [0, 1]
-def scaler(num_layers, thickness_range, wavelength_range, df):
-    thickness_l, thickness_h = thickness_range
-    wavelength_l, wavelength_h = wavelength_range
-    for i in range(1, num_layers+1):
-        df['t_' + str(i)] = (df['t_' + str(i)] - thickness_l) / (thickness_h - thickness_l)
-    df['wavelength'] = (df['wavelength'] - wavelength_l) / (wavelength_h - wavelength_l)
-    return df
-
-# build a dataframe with given properties
-# returns the dataframe and a list of filenames
-def build_dataset(num_layers, paths, thickness_range, wavelength_range, interval, rescale, split):
-    df_all, filenames_all = [], []
-    for path in paths:
-        df, filenames = make_from_JSON(num_layers, path, wavelength_range, interval)
-        df_all.append(df)
-        filenames_all += filenames
-    df_all = pd.concat(df_all, ignore_index=True)
-    if split:
-        df_all = explode(df_all, ['transmission', 'reflection', 'wavelength'])
-    df_all = one_hot_encoding(num_layers, df_all)
-    if rescale:
-        df_all = scaler(num_layers, thickness_range, wavelength_range, df_all)
-    return df_all, filenames_all
-
-# build an iterrable dataset with given properties
-class EntireSpectrumSplit(Dataset):
-    def __init__(self, num_layers, root_dirs, thickness_range=(100,500), wavelength_range=(200,900), interval=5, rescale=True, split=True):
-        self.split = split
-        self.dataframe, self.filenames = build_dataset(num_layers, root_dirs, thickness_range, wavelength_range, interval, rescale, split)
-
-    def __len__(self):
-        return len(self.dataframe)
-
-    def __getitem__(self, idx):
-        row = self.dataframe.loc[idx]
-        transmission = row['transmission']
-        reflection = row['reflection']
-        if self.split:
-            row = torch.Tensor(row.drop(['transmission', 'reflection']))
-        else:
-            row = torch.Tensor(row.drop(['transmission', 'reflection', 'wavelength']))
-        return {'features': row, 'transmission': transmission, 'reflection': reflection}
+      X_train, y_train = load_and_merge_train_validation()
+      train(X_train, y_train, args.ml_method)
+  else:
+    # test a model
+    X_test, y_test, spectrum_names = load_test_data()
+    spectrum_predicteds, spectrum_expecteds, spectrum_rmses, spectrum_maes = test(X_test, y_test, args.ml_method)
+    # Plot the errors. For evaluation models, individual error curves and histograms; for final models, error curve on the entire dataset.
+    if args.final and args.num_layers == 2:
+      plot_error_curve()
+    else:
+      plot_individual_spectrum(spectrum_names, spectrum_predicteds, spectrum_expecteds, spectrum_rmses, spectrum_maes, args.ml_method)
+      plot_error_distribution_histogram(spectrum_rmses, spectrum_maes, args.ml_method)
